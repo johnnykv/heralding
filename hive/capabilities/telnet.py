aaca26c90b215048e086132a56a98ee06b05f9b7
Johnny Vestergaard <jkv@unixcluster.dk>
2013-03-10 21:38:18 +0100

    refactoring to avoid multiple inheritance

--- hive/capabilities/telnet.py
+++ hive/capabilities/telnet.py
@@ -15,40 +15,41 @@
 
 import logging
 
-from hive.models.session import Session
-from telnetsrv.green import TelnetHandler, command
+from telnetsrv.green import TelnetHandler
 
 from handlerbase import HandlerBase
 
 logger = logging.getLogger(__name__)
 
 
-class telnet(HandlerBase, TelnetHandler):
+class telnet(HandlerBase):
+    def __init__(self, sessions, listenport):
+        super(telnet, self).__init__(sessions, listenport)
+
+    def handle_session(self, gsocket, address):
+        session = self.create_session(address, gsocket)
+        telnet_wrapper(address, None, gsocket, session)
+
+
+class telnet_wrapper(TelnetHandler):
+    """
+    Wraps the telnetsrv module to fit the Hive architecture.
+    """
     max_tries = 3
 
     authNeedUser = True
     authNeedPass = True
 
-    #black voodoo to facilitate parents with different __init__ params
-    def __init__(self, *args, **kwargs):
-        if len(args) == 2:
-            #this is the constructor call for HandlerBase
-            sessions = args[0]
-            telnet.sessions = sessions
-            telnet.port = args[1]
-            super(telnet, self).__init__(sessions, telnet.port)
-        elif len(args) == 4:
-            request = args[0]
-            client_address = args[1]
-            server = args[2]
-            #this session is unique for each connection
-            self.session = self.create_session(client_address, args[3])
-            self.auth_count = 0
-            TelnetHandler.__init__(self, request, client_address, server)
+    def __init__(self, client_address, server, socket, session):
+        self.session = session
+        self.auth_count = 0
+        request = telnet_wrapper.false_request()
+        request._sock = socket
+        TelnetHandler.__init__(self, request, client_address, server)
+
 
     def authCallback(self, username, password):
-
-        while self.auth_count < telnet.max_tries:
+        while self.auth_count < telnet_wrapper.max_tries:
             self.session.try_login(username, password)
             self.writeline('Invalid username/password')
             self.auth_count += 1
@@ -57,15 +58,3 @@ class telnet(HandlerBase, TelnetHandler):
 
     def session_end(self):
         self.session.connected = False
-
-    def handle_session(self, gsocket, address):
-        telnet.streamserver_handle(gsocket, address)
-
-    @classmethod
-    def streamserver_handle(cls, socket, address):
-        '''Translate this class for use in a StreamServer'''
-        request = cls.false_request()
-        request._sock = socket
-        server = None
-        cls.logging.debug("Accepted connection, starting telnet session.")
-        cls(request, address, server, socket)
