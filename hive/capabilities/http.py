7d691cff7685d37e25391ff5d6e47c566d2858cb
Aniket Panse <aniketpanse@gmail.com>
2013-03-14 23:24:19 +0530

    added support to pass data to session.try_login() and updated the config file

--- hive/capabilities/http.py
+++ hive/capabilities/http.py
@@ -1,16 +1,28 @@
 import logging
-
 import SimpleHTTPServer
 import SocketServer
+import base64
 
 from handlerbase import HandlerBase
 from BaseHTTPServer import BaseHTTPRequestHandler
 
 class BeeHTTPHandler(BaseHTTPRequestHandler):
 
-    #TODO: Configurable
-    banner = "Microsoft-IIS/5.0"
+    def __init__(self, request, client_address, server, httpsession, options):
 
+        # Had to call parent initializer later, because the methods used
+        # in BaseHTTPRequestHandler.__init__() call handle_one_request()
+        # which calls the do_* methods here. If _banner, _session and _options
+        # are not set, we get a bunch of errors (Undefined reference blah blah)
+        
+        self._options = options
+        if self._options.has_key('banner'):
+            self._banner = self._options['banner']
+        else:
+            self._banner = "Microsoft-IIS/5.0"
+        self._session = httpsession
+        BaseHTTPRequestHandler.__init__(self, request, client_address, server)
+        
     def do_HEAD(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -23,27 +35,32 @@ class BeeHTTPHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        ''' Present frontpage with user authentication. '''
         if self.headers.getheader('Authorization') == None:
             self.do_AUTHHEAD()
             self.wfile.write('<html><b>Unauthorized</b></html>\r\n')
             pass
-        
-        elif self.headers.getheader('Authorization') == 'Basic dGVzdDp0ZXN0':
+
+        # Test Username/password == test/test
+        elif self.headers.getheader('Authorization') == 'Basic dGVzdDp0ZXN00':
             self.do_HEAD()
             self.wfile.write('<html><b>Authenticated!</b></html>\r\n')
             pass
         else:
+            hdr = self.headers.getheader('Authorization')
+            _, enc_uname_pwd = hdr.split(' ')
+            dec_uname_pwd = base64.b64decode(enc_uname_pwd)
+            uname, pwd = dec_uname_pwd.split(':')
+            self._session.try_login(uname, pwd)
             self.do_AUTHHEAD()
             self.wfile.write('<html><b>Access Denied.</b></html>\r\n')
             pass
 
+    def version_string(self):
+        return self._banner
+
     #Disable logging provided by BaseHTTPServer
     def log_message(self, format, *args):
         pass
-
-    def version_string(self):
-        return self.banner
 
 class http(HandlerBase):
     def __init__(self, sessions, options):
@@ -52,4 +69,5 @@ class http(HandlerBase):
 
     def handle_session(self, gsocket, address):
         session = self.create_session(address, gsocket)
-        handler = BeeHTTPHandler(gsocket, address, None)
+        handler = BeeHTTPHandler(gsocket, address, None, httpsession = session,
+                                    options = self._options)
