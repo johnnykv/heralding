a2b207179b144f7dc0c7b25a1cd72c51d9d02bcf
Johnny Vestergaard <jkv@unixcluster.dk>
2013-05-27 09:38:54 +0200

    bootstrapping database

--- beeswarm/beekeeper/db/database_config.py
+++ beeswarm/beekeeper/db/database_config.py
@@ -1,4 +1,6 @@
+import os
 from pony.orm import Database, sql_debug, commit
+import json
 db = None
 
 
@@ -6,6 +8,17 @@ def setup_db(path):
     global db
     db = Database('sqlite', path, create_db=True)
     #This needs to be read from file.
+
+
     from database import Classification
-    c1 = Classification(type='dummy', description_short='dummy classificaiton', description_long='dummy classificaiton')
+    db_path = os.path.dirname(__file__)
+    json_file = open(os.path.join(db_path, 'bootstrap.json'))
+    data = json.load(json_file)
+
+    #making sure that entities does not fall out of scope before commit
+    classifications = {}
+    for entry in data['classifications']:
+        classifications[entry['type']] = Classification(type=entry['type'],
+                                                        description_short=entry['description_short'],
+                                                        description_long=entry['description_long'])
     commit()
