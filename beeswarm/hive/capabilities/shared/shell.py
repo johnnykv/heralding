a16999a2368f2c95df1d0b63b74acf14de7738b7
Aniket Panse <aniketpanse@gmail.com>
2013-06-17 11:34:20 +0530

    added copyright

--- beeswarm/hive/capabilities/shared/shell.py
+++ beeswarm/hive/capabilities/shared/shell.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2013 Aniket Panse <contact@aniketpanse.in>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import argparse
 import os
 from fs.errors import ResourceNotFoundError
@@ -139,7 +154,7 @@ class Commands(TelnetHandler):
                             break
                         self.write(chunk)
             except ResourceNotFoundError:
-                self.writeline('cat: {0}: No such file or directory'.format(filepath)
+                self.writeline('cat: {0}: No such file or directory'.format(filepath))
 
     @command('uptime')
     def command_uptime(self, params):
