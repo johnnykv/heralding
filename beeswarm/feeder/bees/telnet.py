6ad0ccec0ede01fbb5b06ec3b819e71979df3160
Aniket Panse <aniketpanse@gmail.com>
2013-07-18 08:54:18 +0530

    added client commands to telnet capability

--- beeswarm/feeder/bees/telnet.py
+++ beeswarm/feeder/bees/telnet.py
@@ -15,6 +15,7 @@
 
 import logging
 import telnetlib
+import re
 
 from beeswarm.feeder.bees.clientbase import ClientBase
 
@@ -23,6 +24,12 @@ class telnet(ClientBase):
 
     def __init__(self, sessions, options):
         super(telnet, self).__init__(sessions, options)
+        self.client = None
+        self.state = {
+            'last_command': None,
+            'working_dir': '/',
+            'file_list': [],
+        }
 
     def do_session(self, my_ip):
         login = self.options['login']
@@ -35,29 +42,67 @@ class telnet(ClientBase):
             'Sending %s honeybee to %s:%s. (bee id: %s)' % ('telnet', server_host, server_port, session.id))
 
         try:
-            client = telnetlib.Telnet(server_host, server_port)
-            client.set_option_negotiation_callback(self.process_options)
+            self.client = telnetlib.Telnet(server_host, server_port)
+            self.client.set_option_negotiation_callback(self.process_options)
             session.did_connect = True
-            session.source_port = client.sock.getsockname()[1]
-            client.read_until('Username: ')
-            client.write(login + '\r\n')
-            client.read_until('Password: ')
-            client.write(password + '\r\n')
-            current_data = client.read_until('$ ', 5)
+            session.source_port = self.client.sock.getsockname()[1]
+            self.client.read_until('Username: ')
+            self.client.write(login + '\r\n')
+            self.client.read_until('Password: ')
+            self.client.write(password + '\r\n')
+            current_data = self.client.read_until('$ ', 5)
             if not current_data.endswith('$ '):
                 raise InvalidLogin
             session.did_login = True
         except Exception as err:
             logging.debug('Caught exception: %s (%s)' % (err, str(type(err))))
         else:
-            client.write('ls -l\r\n')
-            client.read_until('$ ')
+            self.list()
+            self.list('-l')
             logging.debug('Telnet file listing successful.')
-            client.write('exit\r\n')
-            client.read_all()
-            client.close()
+            self.client.write('exit\r\n')
+            self.client.read_all()
+            self.client.close()
         finally:
             session.alldone = True
+
+    def change_dir(self, params=''):
+        cmd = 'cd {}'.format(params)
+        self.send_command(cmd)
+        data = self.get_response()
+        prompt = data.rsplit('\r\n', 1)[1]
+        pattern = re.compile(r'/[/\w]+')
+        self.state['working_dir'] = pattern.findall(prompt)[0]
+        print self.state['working_dir']
+
+    def echo(self, params=''):
+        cmd = 'cd {}'.format(params)
+        self.send_command(cmd)
+        self.get_response()
+
+    def list(self, params=''):
+        cmd = 'ls {}'.format(params)
+        self.send_command(cmd)
+        resp = self.get_response()
+        resp = resp.split('\r\n')
+        if params:
+            # Our Hive capability only accepts "ls -l" or "ls" so params will always be "-l"
+            files = []
+            for line in resp[2:-1]:  # Discard the line with echoed command, total and prompt
+                # 8 Makes sure we have the right result even if filenames have spaces.
+                filename = line.split(' ', 8)[-1]
+                files.append(filename)
+            self.state['file_list'] = files
+        else:
+            resp = '\r\n'.join(resp[1:-1])
+            self.state['file_list'] = resp.split()
+
+    def get_response(self):
+        response = self.client.read_until('$ ', 5)
+        return response
+
+    def send_command(self, cmd):
+        self.client.write(cmd + '\r\n')
 
     def process_options(self, *args):
         """Dummy callback, used to disable options negotiations"""
