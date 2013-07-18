37b236a3f70eff73000adc8f6744e9371e65650a
Aniket Panse <aniketpanse@gmail.com>
2013-07-18 19:49:27 +0530

    added test cases for telnet bee

--- beeswarm/feeder/tests/test_telnet.py
+++ beeswarm/feeder/tests/test_telnet.py
@@ -12,6 +12,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import telnetlib
 
 import gevent.monkey
 gevent.monkey.patch_all()
@@ -76,6 +77,62 @@ class Telnet_Test(unittest.TestCase):
         self.assertEquals(session.did_login, True)
 
         srv.stop()
+
+    def test_commands(self):
+        sessions = {}
+        users = {'test': HiveUser('test', 'test')}
+        authenticator = Authenticator(users)
+        Session.authenticator = authenticator
+
+        cap = hive_telnet.telnet(sessions, {'enabled': 'True', 'port': 0, 'max_attempts': 3}, users, self.work_dir)
+        socket = create_socket(('0.0.0.0', 0))
+        srv = StreamServer(socket, cap.handle_session)
+        srv.start()
+
+        bee_info = {
+            'timing': 'regular',
+            'login': 'test',
+            'password': 'test',
+            'port': srv.server_port,
+            'server': '127.0.0.1'
+        }
+        beesessions = {}
+
+        BeeSession.feeder_id = 'f51171df-c8f6-4af4-86c0-f4e163cf69e8'
+        current_bee = bee_telnet.telnet(beesessions, bee_info)
+
+        current_bee.connect()
+        current_bee.login(bee_info['login'], bee_info['password'])
+
+        # Command: cd
+        self.assertEquals('/', current_bee.state['working_dir'])
+        current_bee.change_dir('/var')
+        self.assertEquals('/var', current_bee.state['working_dir'])
+
+        # Command: pwd
+        resp = current_bee.pwd()
+        self.assertTrue('/var' in resp)
+
+        # Command: uname
+        resp = current_bee.uname('-o')
+        self.assertTrue('GNU/Linux' in resp)
+
+        # Command: cat
+        resp = current_bee.cat('/var/www/index.html')
+        self.assertTrue('</html>' in resp)
+
+        # Command: uptime
+        resp = current_bee.uptime('-V')
+        self.assertTrue('procps version 3.2.8' in resp)
+
+        # Command: echo
+        resp = current_bee.echo('just testing!')
+        self.assertTrue('just testing!' in resp)
+
+        # Command: list
+        resp = current_bee.list()
+        self.assertTrue('var' in resp)
+
 
 if __name__ == '__main__':
     unittest.main()
