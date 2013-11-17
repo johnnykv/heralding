a5b4611edd3af757fe372a9817ac8c553be44f83
Johnny Vestergaard <jkv@unixcluster.dk>
2013-11-17 20:52:54 +0100

    initial support for displaying of command transcripts
    #152

--- beeswarm/beekeeper/webapp/app.py
+++ beeswarm/beekeeper/webapp/app.py
@@ -565,7 +565,7 @@ def data_sessions_attacks(_type):
     for a in entries:
         classification = a.classification_id.replace('_', ' ').capitalize()
         row = {'time': a.timestamp.strftime('%Y-%m-%d %H:%M:%S'), 'protocol': a.protocol, 'ip_address': a.source_ip,
-               'classification': classification}
+               'classification': classification, 'id': a.id}
         rows.append(row)
     rsp = Response(response=json.dumps(rows, indent=4), status=200, mimetype='application/json')
     return rsp
