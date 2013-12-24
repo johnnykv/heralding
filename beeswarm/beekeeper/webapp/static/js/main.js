efddd82bfce7f908084bb5b22f9a2a4ff94436c6
Johnny Vestergaard <jkv@unixcluster.dk>
2013-12-24 20:41:33 +0100

    fixed pagination issue

--- beeswarm/beekeeper/webapp/static/js/main.js
+++ beeswarm/beekeeper/webapp/static/js/main.js
@@ -22,7 +22,7 @@ GenericDataSource.prototype = {
 				var startIndex = options.pageIndex * options.pageSize;
 				var endIndex = startIndex + options.pageSize;
 				var end = (endIndex > count) ? count : endIndex;
-				var pages = count / options.pageSize;
+				var pages = Math.ceil(count / options.pageSize);
 				var page = options.pageIndex + 1;
 				var start = startIndex + 1;
                 // TODO: slice and dice server side
