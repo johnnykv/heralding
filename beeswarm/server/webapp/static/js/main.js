8ba26c94c213f77785fc5c53ba6999ca4bac53e8
Johnny Vestergaard <jkv@unixcluster.dk>
2014-06-16 21:43:08 +0200

    generalisation of datasource

--- beeswarm/server/webapp/static/js/main.js
+++ beeswarm/server/webapp/static/js/main.js
@@ -32,3 +32,42 @@ GenericDataSource.prototype = {
 			});
 	}
 };
+
+var FlexibleUrlDatasource = function (options) {
+	this._formatter = options.formatter;
+	this._columns = options.columns;
+    this._url = options.url;
+    this._formatter = options.formatter;
+
+    this._url_formatter = options.url_formatter;
+
+};
+
+FlexibleUrlDatasource.prototype = {
+
+	columns: function () {
+		return this._columns;
+	},
+
+	data: function (options, callback) {
+        var self = this;
+        var url = self._url_formatter();
+		$.ajax(url, {
+            dataType: 'json',
+			type: 'GET'
+			}).done(function (response) {
+				var data = response;
+				var count = response.length;
+				var startIndex = options.pageIndex * options.pageSize;
+				var endIndex = startIndex + options.pageSize;
+				var end = (endIndex > count) ? count : endIndex;
+				var pages = Math.ceil(count / options.pageSize);
+				var page = options.pageIndex + 1;
+				var start = startIndex + 1;
+                // TODO: slice and dice server side
+                data = data.slice(startIndex, endIndex);
+                if (self._formatter) self._formatter(data);
+                callback({ data: data, start: start, end: end, count: count, pages: pages, page: page });
+			});
+	}
+};
