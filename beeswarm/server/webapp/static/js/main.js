e6828f453e072155875bd5c1b07619bcc1cae9c6
Johnny Vestergaard <jkv@unixcluster.dk>
2014-06-17 19:50:43 +0200

    use new datasource for all datagrids

--- beeswarm/server/webapp/static/js/main.js
+++ beeswarm/server/webapp/static/js/main.js
@@ -1,73 +1,43 @@
-var GenericDataSource = function (options) {
-	this._formatter = options.formatter;
-	this._columns = options.columns;
-    this._url = options.url;
-    this._formatter = options.formatter;
-};
-
-GenericDataSource.prototype = {
-
-	columns: function () {
-		return this._columns;
-	},
-
-	data: function (options, callback) {
-        var self = this;
-		$.ajax(self._url, {
-            dataType: 'json',
-			type: 'GET'
-			}).done(function (response) {
-				var data = response;
-				var count = response.length;
-				var startIndex = options.pageIndex * options.pageSize;
-				var endIndex = startIndex + options.pageSize;
-				var end = (endIndex > count) ? count : endIndex;
-				var pages = Math.ceil(count / options.pageSize);
-				var page = options.pageIndex + 1;
-				var start = startIndex + 1;
-                // TODO: slice and dice server side
-                data = data.slice(startIndex, endIndex);
-                if (self._formatter) self._formatter(data);
-                callback({ data: data, start: start, end: end, count: count, pages: pages, page: page });
-			});
-	}
-};
-
 var FlexibleUrlDatasource = function (options) {
-	this._formatter = options.formatter;
-	this._columns = options.columns;
-    this._url = options.url;
+    this._formatter = options.formatter;
+    this._columns = options.columns;
     this._formatter = options.formatter;
 
-    this._url_formatter = options.url_formatter;
+    this._url = options.url;
 
 };
 
 FlexibleUrlDatasource.prototype = {
 
-	columns: function () {
-		return this._columns;
-	},
+    columns: function () {
+        return this._columns;
+    },
 
-	data: function (options, callback) {
+    data: function (options, callback) {
         var self = this;
-        var url = self._url_formatter();
-		$.ajax(url, {
+        if (jQuery.isFunction(self._url)) {
+            var url = this._url()
+        }
+        else {
+            var url = this._url;
+        }
+
+        $.ajax(url, {
             dataType: 'json',
-			type: 'GET'
-			}).done(function (response) {
-				var data = response;
-				var count = response.length;
-				var startIndex = options.pageIndex * options.pageSize;
-				var endIndex = startIndex + options.pageSize;
-				var end = (endIndex > count) ? count : endIndex;
-				var pages = Math.ceil(count / options.pageSize);
-				var page = options.pageIndex + 1;
-				var start = startIndex + 1;
-                // TODO: slice and dice server side
-                data = data.slice(startIndex, endIndex);
-                if (self._formatter) self._formatter(data);
-                callback({ data: data, start: start, end: end, count: count, pages: pages, page: page });
-			});
-	}
+            type: 'GET'
+        }).done(function (response) {
+            var data = response;
+            var count = response.length;
+            var startIndex = options.pageIndex * options.pageSize;
+            var endIndex = startIndex + options.pageSize;
+            var end = (endIndex > count) ? count : endIndex;
+            var pages = Math.ceil(count / options.pageSize);
+            var page = options.pageIndex + 1;
+            var start = startIndex + 1;
+            // TODO: slice and dice server side
+            data = data.slice(startIndex, endIndex);
+            if (self._formatter) self._formatter(data);
+            callback({ data: data, start: start, end: end, count: count, pages: pages, page: page });
+        });
+    }
 };
