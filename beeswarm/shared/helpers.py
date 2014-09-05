0c04a7b74f93316bea67fc29b0a9e453f93dd149
Johnny Vestergaard <jkv@unixcluster.dk>
2014-09-05 23:11:59 +0200

    fixed test

--- beeswarm/shared/helpers.py
+++ beeswarm/shared/helpers.py
@@ -259,13 +259,18 @@ def stop_if_not_write_workdir(dir):
     if not os.access(dir, os.W_OK | os.X_OK):
         logger.error('Beeswarm needs write permisison to the work directory, '
                      'but did not have write permission to directory {0}.'.format(dir))
-        logger.debug('Current workdir: {0}, Asked dir: {0}'.format(os.getcwd(), dir))
-        logger.debug('Files in directory: {0}'.format(os.listdir(os.getcwd())))
+        logger.debug('Current workdir: {0}, Asked dir: {1}'.format(os.getcwd(), dir))
+        logger.debug('Files in directory: {0}'.format(os.listdir(dir)))
         sys.exit(1)
-    for _file in os.listdir(dir):
-        if not os.access(_file, os.W_OK):
+    for item in os.listdir(dir):
+        error = False
+        if os.path.isfile(item):
+            if not os.access(item, os.W_OK):
+                error = True
+        elif os.path.isdir(item):
+            if not os.access(item, os.W_OK | os.X_OK):
+                error = True
+        if error:
             logger.error('Beeswarm needs write permisison to all files in the the work directory, '
-                        'but did not have write permission to {0}.'.format(_file))
-            logger.debug('Current workdir: {0}, Asked dir: {0}'.format(os.getcwd(), dir))
-            logger.debug('Files in directory: {0}'.format(os.listdir(os.getcwd())))
+                        'but did not have write permission to {0}.'.format(item))
             sys.exit(1)
