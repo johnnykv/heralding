bba7a0edce200e7cba7f9116beb2de31953f3b24
Johnny Vestergaard <jkv@unixcluster.dk>
2015-01-04 11:48:11 +0100

    removed some unused stuff

--- tools/live-build/build_iso.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-mkdir config/includes.binary
-echo " * Creating dummy configuration file."
-tr '\000' '\007' < /dev/zero | dd of=config/includes.binary/dummy.tar.gz bs=1024 count=10k
-echo " * Starting build of ISO file"
-lb build
-mv binary.hybrid.iso beeswarm_client.iso
-echo " * Beeswarm client ISO has been generated and saved as beeswarm_client.iso"
