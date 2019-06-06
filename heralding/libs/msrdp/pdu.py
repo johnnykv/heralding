7000cae61e38012ac924b824b94c8597e9392632
realsdx <realsdx@protonmail.com>
2019-06-07 01:36:30 +0530

    add decryption functions

--- heralding/libs/msrdp/pdu.py
+++ heralding/libs/msrdp/pdu.py
@@ -18,8 +18,9 @@
 
 from .packer import Uint16BE, Int16BE, Uint32LE, Uint32BE
 from .parser import x224ConnectionRequestPDU
+from .security import ServerSecurity
 
-
+    
 class tpktPDU:
     def __init__(self, payload):
         self.version = 3
@@ -63,7 +64,7 @@ class x224ConnectionConfirmPDU():
 
 class ServerData:
     @classmethod
-    def generate(cls, cReqproto):
+    def generate(cls, cReqproto, serverSec):
         # Servr Core data
         coreData_1 = b'\x01\x0c\x0c\x00\x04\x00\x08\x00'
         clientReqPro = Uint32LE.pack(cReqproto)
@@ -72,21 +73,29 @@ class ServerData:
         netData = b'\x03\x0c\x08\x00\xeb\x03\x00\x00'  # here channel count 0
 
         # ServerSec Data
+        # here choosing encryption of 128bit by default method \x02\x00\x00\x00
         part_1 = b'\x02\x0c\xec\x00\x02\x00\x00\x00\x03\x00\x00\x00 \x00\x00\x00\xb8\x00\x00\x00'
-        serverRandom = b'\xf9\xdf\xc4p\x8a\x08\xcds5Q:\xa3!_\x8f\xa1\xeeQ\xd6Nj\x95Zy0\x12\xbf\xf5&\xfb!('
+        # serverRandom = b'\xf9\xdf\xc4p\x8a\x08\xcds5Q:\xa3!_\x8f\xa1\xeeQ\xd6Nj\x95Zy0\x12\xbf\xf5&\xfb!('
+        serverRandom = ServerSecurity.SERVER_RANDOM
 
         ## ServerCert ##
-        pubkeyProps_1 = b'\x01\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x06\x00\x5c\x00'
-        pubkeyProps_2 = b'RSA1H\x00\x00\x00\x00\x02\x00\x00?\x00\x00\x00'
-        pubExp = b'\x01\x00\x01\x00'
-        modulus = b'g3\x9c\xd5\x94\xa0\tO\xbfrVt`\xab\x7f\xf1+\xf7J\x86\xa3(ZK\x12\xc44(\xcd\xec"E6a\xf3\x1d&\t\x8fL\xb6\xc8f\x9c\xd0\xa3\xaf8\xaa\xe1\xfb\xcb\\\r\xfc\xbb\xd8\x93\x1cZ\xce\x1b\x8f\x92\x00\x00\x00\x00\x00\x00\x00\x00'
-        sigBlobProps = b'\x08\x00\x48\x00'
-        sigBlob = b';U3\x0c8r\x91 \xa9b^l\xa4\x1f\xcd\n\x9c\x1b\xbb\x13\xcdge\x19\xd4\x1e\xc7\x89\x8e\x9a\x98\xb4z\x02\xf0\xdc]\x1dh\x8d\xfa\x01Ah\x9c\xda\xe9\xdf\xabw\xf2\xaf\x90\x02\xe7\xd1\xe4\xaa\xa1\x0e\xcc\x03\xd3B\x00\x00\x00\x00\x00\x00\x00\x00'
-
+        # pubkeyProps_1 = b'\x01\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x06\x00\x5c\x00'
+        # pubkeyProps_1 = SecurityInfo.SERVER_PUBKEY_PROPS_1
+        # pubkeyProps_2 = SecurityInfo.SERVER_PUBKEY_PROPS_2
+        # pubExp = SecurityInfo.SERVER_PUBLIC_EXPONENT
+        # # modulus = b'g3\x9c\xd5\x94\xa0\tO\xbfrVt`\xab\x7f\xf1+\xf7J\x86\xa3(ZK\x12\xc44(\xcd\xec"E6a\xf3\x1d&\t\x8fL\xb6\xc8f\x9c\xd0\xa3\xaf8\xaa\xe1\xfb\xcb\\\r\xfc\xbb\xd8\x93\x1cZ\xce\x1b\x8f\x92\x00\x00\x00\x00\x00\x00\x00\x00'
+        # modulus = SecurityInfo.SERVER_MODULUS
+        # sigBlobProps = b'\x08\x00\x48\x00'
+        # sigBlob = b';U3\x0c8r\x91 \xa9b^l\xa4\x1f\xcd\n\x9c\x1b\xbb\x13\xcdge\x19\xd4\x1e\xc7\x89\x8e\x9a\x98\xb4z\x02\xf0\xdc]\x1dh\x8d\xfa\x01Ah\x9c\xda\xe9\xdf\xabw\xf2\xaf\x90\x02\xe7\xd1\xe4\xaa\xa1\x0e\xcc\x03\xd3B\x00\x00\x00\x00\x00\x00\x00\x00'
+        
+        # an instance of ServerSecurity class required
+        certData = serverSec.getServerCertBytes()
+        print("CERT_DATA: ", repr(certData))
         #ADD all
         serverCoreData = coreData_1+clientReqPro
         serverNetData = netData
-        serverSecData = part_1+serverRandom+pubkeyProps_1+pubkeyProps_2+pubExp+modulus+sigBlobProps+sigBlob
+        # serverSecData = part_1+serverRandom+pubkeyProps_1+pubkeyProps_2+pubExp+modulus+sigBlobProps+sigBlob
+        serverSecData = part_1+serverRandom+certData
 
         return serverCoreData+serverNetData+serverSecData
 
@@ -94,11 +103,12 @@ class ServerData:
 class MCSConnectResponsePDU():
     """ Server MCS Connect Response PDU with GCC Conference Create Response """
 
-    def __init__(self, cReqproto):
+    def __init__(self, cReqproto, serverSec):
         self.cReqproto = cReqproto
+        self.serverSec = serverSec
 
     def generate(self):
-        serverData = ServerData.generate(self.cReqproto)
+        serverData = ServerData.generate(self.cReqproto, self.serverSec)
         serverDataLen = Uint16BE.pack(len(serverData) | 0x8000)
         gccCreateRes = b'\x00\x05\x00\x14\x7c\x00\x01\x2a\x14\x76\x0a\x01\x01\x00\x01\xc0\x00\x4d\x63\x44\x6e'
         cc_userDataLen = Uint16BE.pack(len(serverData)+2+len(gccCreateRes))
